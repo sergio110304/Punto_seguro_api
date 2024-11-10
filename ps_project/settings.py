58e71ee932c92bc479cd957a6c8e1e248b3929f1
Sergio Rodriguez <srodriguezcabana@gmail.com>
2024-11-10 07:46:58 -0500

    Add print

--- ps_project/settings.py
+++ ps_project/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
-
+print("DJANGO_ALLOWED_HOSTS:", os.getenv("DJANGO_ALLOWED_HOSTS"))
 
 # Application definition
 
